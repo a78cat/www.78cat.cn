cecdd9c81220116e6a534153ee1d318fb6407c6a
shrewdyu2001 <shrewdyu2001@qq.com>
2022-12-08 16:01:33 +0800

    添加了备案号，修改了icon源为阿里icon库

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?814de5867a80b547731582a77433839b";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
