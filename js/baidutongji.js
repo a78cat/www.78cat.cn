ad717be68a35f0ab0c2c6335660c663fac685155
a78cat <shrewdyu2001@qq.com>
2023-05-21 12:53:11 +0800

    添加了一堆链接，但是脑瘫审美

--- js/baidutongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?814de5867a80b547731582a77433839b";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
