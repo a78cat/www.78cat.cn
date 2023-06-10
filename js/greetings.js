1e6b5b21a5dd7397c4f57145f726e75b8fdd3fa0
78cat <188687820@qq.com>
2023-06-10 14:15:24 +0800

    -优化了问候的代码

--- js/greetings.js
+++ js/greetings.js
@@ -1,8 +1,8 @@
-const midnight = 0;
-const morning = 4;
-const midday = 10;
-const afternoon = 15;
-const night = 19;
+const midnight_hour = 0;
+const morning_hour = 4;
+const midday_hour = 10;
+const afternoon_hour = 15;
+const night_hour = 19;
 //获取当前小时数(0-23)
 var now_hour = String(new Date().getHours()).padStart(2, '0');
 //获取页面问候元素
@@ -10,13 +10,13 @@ var greetings = document.getElementById('greetings');
 //记录想修改的问候
 let greeting_text = '';
 //设置规则
-if (midnight <= now_hour && now_hour < morning) {
+if (midnight_hour <= now_hour && now_hour < morning_hour) {
     greeting_text = '大半夜的，还tm不睡'
-} else if (morning <= now_hour && now_hour < midday) {
+} else if (morning_hour <= now_hour && now_hour < midday_hour) {
     greeting_text = '早安！一日之计在于晨'
-} else if (midday <= now_hour && now_hour < afternoon) {
+} else if (midday_hour <= now_hour && now_hour < afternoon_hour) {
     greeting_text = '一转眼就到中午了'
-} else if (afternoon <= now_hour && now_hour < night) {
+} else if (afternoon_hour <= now_hour && now_hour < night_hour) {
     greeting_text = '下午记得睡个午觉'
 } else if (night <= now_hour && now_hour < 24) {
     greeting_text = '晚上记得早点睡'
